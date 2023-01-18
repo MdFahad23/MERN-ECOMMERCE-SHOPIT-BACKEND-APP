9bfe40a3b96eb0c706a1d20d4287e336a4a3d598
Md Fahad <fahadbdctglh8@gmail.com>
2023-01-18 15:17:49 +0600

    Creat error handler

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+require("express-async-errors");
 const express = require("express");
 const app = express();
+const error = require("./middleware/error");
 
 // All middleware
 require("./middleware")(app);
 require("./middleware/routes")(app);
+
+// Error Handler
+app.use(error);
 
 module.exports = app;
