091b4c17e92a64dd337cf10c85f1e27d4ea28857
Md Fahad <fahadbdctglh8@gmail.com>
2023-01-23 15:41:52 +0600

    Product Update and Delete

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -22,11 +22,10 @@ module.exports.createProduct = async (req, res) => {
     //   Create new Product
     let product = new Product(fields);
 
-    //   Cake this photo
+    //   Check photo
     if (files.photo) {
       fs.readFile(files.photo.filepath, (err, data) => {
         if (err) return res.status(400).send("problem in file data!");
-        console.log(files);
         product.photo.data = data;
         product.photo.contentType = files.photo.mimetype;
         product.save((err, result) => {
@@ -71,7 +70,7 @@ module.exports.getProducts = async (req, res) => {
   else return res.status(200).send(product);
 };
 
-// Get ProductById
+// Get ProductById(admin)
 module.exports.getProductById = async (req, res) => {
   let productId = req.params.id;
   const product = await Product.findById(productId)
@@ -92,4 +91,62 @@ module.exports.getPhoto = async (req, res) => {
   res.set("Content-Type", product.photo.contentType);
   if (!product) return res.status(404).send("Product Image Not Found!");
   else return res.status(200).send(product.photo.data);
+};
+
+// Update Product(admin)
+module.exports.updateProduct = async (req, res) => {
+  // Query Product
+  let product = await Product.findById(req.params.id);
+  // Create from
+  let from = new formidable.IncomingForm();
+
+  // form to parse
+  from.parse(req, (err, fields, files) => {
+    if (err) return res.status(400).send(err.message);
+    //   Update Fields
+    let updateField = _.pick(fields, [
+      "name",
+      "description",
+      "price",
+      "category",
+      "quantity",
+    ]);
+    updateField.user = req.user._id;
+    _.assignIn(product, updateField);
+
+    // Check Photo
+    if (files.photo) {
+      fs.readFile(files.photo.filepath, (err, data) => {
+        if (err) return res.status(400).send("problem in file data!");
+        product.photo.data = data;
+        product.photo.contentType = files.photo.mimetype;
+        product.save((err, result) => {
+          if (err) return res.status(500).send("Internal Server error!");
+          else
+            return res
+              .status(200)
+              .send({ message: "Product Update Successfully!" });
+        });
+      });
+    } else {
+      product.save((err, result) => {
+        if (err) return res.status(500).send("Internal Server error!");
+        else
+          return res
+            .status(200)
+            .send({ message: "Product Update Successfully!" });
+      });
+    }
+  });
+};
+
+// Delete Product(admin)
+module.exports.deleteProduct = async (req, res) => {
+  let product = await Product.findById(req.params.id);
+  if (!product) return res.status(400).send("Product not Found!");
+  await product.remove();
+  return res.status(200).send({
+    success: true,
+    message: "Product Delete Successfully!",
+  });
 };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,13 +5,19 @@ const {
   getProducts,
   getProductById,
   getPhoto,
+  updateProduct,
+  deleteProduct,
 } = require("../controller/product.controller");
 const authorize = require("../middleware/authorize");
 const admin = require("../middleware/admin");
 
 router.route("/admin/product/new").post(authorize, admin, createProduct);
 router.route("/products").get(getProducts);
-router.route("/product/:id").get(getProductById);
+router
+  .route("/admin/product/:id")
+  .get(authorize, admin, getProductById)
+  .delete(authorize, admin, deleteProduct);
 router.route("/product/photo/:id").get(getPhoto);
+router.route("/admin/product/update/:id").put(authorize, admin, updateProduct);
 
 module.exports = router;
