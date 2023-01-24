8e2ceb2df3fb70a374a730ba734eb9dd28563146
Md Fahad <fahadbdctglh8@gmail.com>
2023-01-24 12:37:38 +0600

    Change Routes file

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -13,11 +13,13 @@ const authorize = require("../middleware/authorize");
 const admin = require("../middleware/admin");
 
 router.route("/admin/product/new").post(authorize, admin, createProduct);
-router.route("/products").get(getProducts);
+router
+  .route("/products")
+  .get(getProducts)
+  .get(authorize, admin, getAdminProducts);
 router
   .route("/admin/product/:id")
   .get(authorize, admin, getProductById)
-  .get(authorize, admin, getAdminProducts)
   .delete(authorize, admin, deleteProduct);
 router.route("/product/photo/:id").get(getPhoto);
 router.route("/admin/product/update/:id").put(authorize, admin, updateProduct);
